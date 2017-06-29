584280b5a2fe9ad304302cdcbd2d0496018d416a
Nick Drewe <nick.drewe@gmail.com>
2017-06-29 16:01:17 +1000

    remove <pre> from allowed tags

--- actions/process_email.js
+++ actions/process_email.js
@@ -10,7 +10,7 @@ const sanitizeOptions = {
   // 'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img' ],
   allowedTags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
   'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-  'thead', 'caption', 'pre', 'img', 'dir', 'marquee', 'font' ],
+  'thead', 'caption', 'img', 'dir', 'marquee', 'font' ],
   allowedAttributes: {
     a: [ 'href', 'name', 'target', 'rel' ],
     // We don't currently allow img itself by default, but this
