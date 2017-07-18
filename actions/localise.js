f1b3eca46c4b6bb108b6ce88293d98b7b8a3b03a
Nick Drewe <nick.drewe@gmail.com>
2017-07-18 16:18:37 +1000

    language pref fix

--- actions/localise.js
+++ actions/localise.js
@@ -13,18 +13,25 @@ const acceptLanguages = [
 // falls-back to english
 const useLanguage = req => {
   const acceptedLanguage = req.acceptsLanguages(acceptLanguages)
-  console.log(acceptedLanguage)
-  if(acceptedLanguage){
+  if(acceptedLanguage && !acceptedLanguage.isArray){
+
     var lang = acceptedLanguage.substring(0,2) // take the first two characters only
 
     // traditional chinese
     if(['zh-hk', 'zh-sg', 'zh-tw'].includes(acceptedLanguage)){
       lang = acceptedLanguage
     }
-
-    return(lang)
+    return lang
+  }else if(acceptedLanguage && !acceptedLanguage.isArray){
+    // prioritise EN if multiple languages are accepted
+    if(acceptedLanguage.includes('en')){
+      lang = 'en'
+    }else{
+      lang = acceptedLanguage[0]
+    }
+    return lang
   }else{
-    return('en')
+    return 'en'
   }
 }
 
